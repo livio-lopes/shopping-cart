72fc5408f504266e595ba6b9bcf5ed230e556ca0
Lívio Lopes <antonio.lvio760@gmail.com>
2022-11-17 17:36:24 -0300

    primeiros testes escritos para fetchProductList

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -8,14 +8,22 @@ describe('Teste a função fetchProductsList', () => {
     expect(typeof fetchProductsList).toBe('function')
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    const listaComputadores = await fetchProductsList('computador')
+    expect(fetch).toHaveBeenCalledTimes(1);
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const listaComputadores = await fetchProductsList('computador')
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    expect(listaComputadores.url).toBe(url)
   });
 
-  // it('...', () => {
-  // });
+  it('fetchProductList retorna lista de computadores', async () => {
+    const listaComputadores = await fetchProductsList('computador')
+    expect(listaComputadores).toEqual(computadorSearch)
+  });
+  it(`se fetchProductList lança mensagem de erro 'Termo de busca não informado' quando executada sem argumento`, async () => {
+    expect(await fetchProductsList()).toThrow('Termo de busca não informado')
+  })
 });
