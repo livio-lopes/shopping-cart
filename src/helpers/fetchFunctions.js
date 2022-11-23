b1224c35759d6fb1340bcb822f57dda612dfd0a4
Lívio Lopes <antonio.lvio760@gmail.com>
2022-11-23 13:21:03 -0300

    corrigindo teste e resolvendo requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,14 @@
+
 export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
+export const fetchProductsList = async (QUERY) => {
   // seu código aqui
+  if (!QUERY) throw new Error('Termo de busca não informado');
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+  const requisition = await fetch(URL)
+    .then((response) => response.json())
+    .then((data) => data.results);
+  return requisition;
 };
