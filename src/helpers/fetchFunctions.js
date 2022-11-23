fb4696aeb9a17c99da526dccf7f298b589005cd8
Lívio Lopes <antonio.lvio760@gmail.com>
2022-11-23 12:30:51 -0300

    Revert "requisito 2 resolvido"
    This reverts commit 082d7d7eddaab09a56753b856b6fcd464986c1ba.

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,12 +2,6 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = async (QUERY) => {
+export const fetchProductsList = () => {
   // seu código aqui
-  if (!QUERY) throw new Error('Termo de busca não informado');
-  const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
-  const requisition = await fetch(URL)
-    .then((response) => response.json())
-    .then((data) => data.results);
-  return requisition;
 };
