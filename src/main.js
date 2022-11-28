6e1d40131b404ca9dfc9b0d7b2ae3481d75072bf
Lívio Lopes <antonio.lvio760@gmail.com>
2022-11-28 17:48:05 -0300

    requisito 3 resolvido

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const listComputers = await fetchProductsList('computador');
+const listProducts = document.querySelector('.products');
+
+listComputers.forEach((computer) => {
+  const product = {
+    id: computer.id,
+    title: computer.title,
+    thumbnail: computer.thumbnail,
+    price: computer.price,
+  };
+  const item = createProductElement(product);
+  listProducts.appendChild(item);
+});
