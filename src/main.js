5d5285df607939026e2ec9c29fb369de72b13b46
Lívio Lopes <antonio.lvio760@gmail.com>
2022-11-29 08:52:09 -0300

    requisito 4 resolvido

--- src/main.js
+++ src/main.js
@@ -3,11 +3,23 @@ import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 
+function loading() {
+  const container = document.querySelector('.container');
+  const msgLoading = document.createElement('h2');
+  msgLoading.id = 'msgLoading';
+  msgLoading.innerHTML = 'carregando...';
+  container.appendChild(msgLoading);
+}
+function load() {
+  const msgLoading = document.querySelector('#msgLoading');
+  msgLoading.innerHTML = '';
+}
+
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-
-const listComputers = await fetchProductsList('computador');
 const listProducts = document.querySelector('.products');
-
+loading();
+const listComputers = await fetchProductsList('computador');
+load();
 listComputers.forEach((computer) => {
   const product = {
     id: computer.id,
