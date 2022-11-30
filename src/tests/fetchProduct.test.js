90a2bf0876b2391cc59ab8b0db5e80ee69437619
Lívio Lopes <antonio.lvio760@gmail.com>
2022-11-29 23:05:24 -0300

    requisito 6 resolvido

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,25 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('fetchProduct é uma função', () => {
+    expect(typeof fetchProduct).toBe('function')
   });
+  it('fetch é chamado ao executar fetchProducts', async () => {
+    const sandisk = await fetchProduct('MLB1405519561')
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  it('fetch é chamado com o endpoint correto ao executar fetchProducts', async () => {
+    const sandisk = await fetchProduct('MLB1405519561');
+    const URL = 'https://api.mercadolibre.com/items/MLB1405519561'
+    expect(fetch).toHaveBeenCalledWith(URL);
+  });
+  it(`fetchProduct('MLB1405519561')  retorna informações do produto`, async () => {
+    const sandisk = await fetchProduct('MLB1405519561')
+    expect(sandisk).toEqual(product)
+  });
+
+  it(`fetchProduct() lança mensagem de erro 'Termo de busca não informado' quando executada sem argumento`,    async () =>  {
+    await expect(fetchProducts()).rejects.toThrow(/^Termo de busca não informado$/);
+  })
 });
